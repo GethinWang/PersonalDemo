0d7bab892c914c4d589d91a102abc8367c26975d
wanggx <wanggx@startimes.com.cn>
2019-07-30 17:42:49 +0800

    git回退测试3

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,5 +4,9 @@
 class Solution {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
     }
 }
